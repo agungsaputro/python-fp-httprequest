d8185cc4ed1062c0d89e42443cf428e911804a80
agungsaputro <agungsaputro484@gmail.com>
2020-09-07 07:18:48 +0700

    update

--- 05-Combine.py
+++ 05-Combine.py
@@ -3,10 +3,17 @@ import json
 
 urlPost = "https://jsonplaceholder.typicode.com/posts"
 urlUser = "https://jsonplaceholder.typicode.com/users"
-post = requests.get(urlPost).json()
-user = requests.get(urlUser).json()
 
-for p in post:
-    p['user'] = [u for u in user if u['id'] == p['userId']] 
-    dumps = json.dumps(p, indent=6)
-    print(dumps)
+
+def combine (urlPost,urlUser):
+    post = requests.get(urlPost).json()
+    user = requests.get(urlUser).json()
+
+    for p in post:
+        p['user'] = [u for u in user if u['id'] == p['userId']] 
+        result = json.dumps(p, indent=6)
+        print(result)
+    
+    return combine
+
+combine(urlPost,urlUser)
